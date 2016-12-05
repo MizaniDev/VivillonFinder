445e21105598407eec0c5db42091fdd9577df273
Rodrigo Mizani <rodrigo.mizani@gmail.com>
2016-12-05 21:05:47 -0200

    Initial commit - Criação do projeto

---
+++ app/src/main/java/vivillontrade/com/br/MainAPP.java
@@ -0,0 +1,13 @@
+package vivillontrade.com.br;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainAPP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_main);
+    }
+}
